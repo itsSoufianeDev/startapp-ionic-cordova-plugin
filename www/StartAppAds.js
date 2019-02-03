c67074e86f679389b45c5d457e188c492f76e6c5
Soufiane Mgani <550hp.engine@gmail.com>
2019-02-03 22:39:50 +0100

    init

--- www/StartAppAds.js
+++ www/StartAppAds.js
@@ -3,18 +3,33 @@ var exec = require('cordova/exec');
 
 function Startapp () {}
 
+/**
+Initialize SDK
+**/
 Startapp.prototype.init = (args, success, error) => {
 	exec(success, error, 'init', [args]);
 }
 
+
+/**
+Set User Consent: GDRP for EU Users
+**/
 Startapp.prototype.setConsent = (args, success, error) => {
 	exec(success, error, 'setConsent', [args]);
 }
 
+
+/**
+Load an Interstitial Ad
+**/
 Startapp.prototype.loadInterstitial = (success, error) => {
 	exec(success, error, 'loadInterstitial', []);
 }
 
+
+/**
+Load a Rewarded Video Ad
+**/
 Startapp.prototype.loadRewardedVideo = (success, error) => {
 	exec(success, error, 'loadRewardedVideo', []);
 }
